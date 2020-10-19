8abd618481d2bfea138c4773073e800a86026516
Rishi Balamurugan <rishi.b2372@gmail.com>
2020-10-19 19:07:43 +0530

    First coneection with firebase

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -41,15 +41,19 @@ class _HomeScreenState extends State<HomeScreen> {
           if (!snapshot.hasData ||
               snapshot.hasError ||
               snapshot.connectionState == ConnectionState.waiting) {
+              
             return Center(
               child: Container(
                 width: ScreenUtil().setWidth(20),
                 height: ScreenUtil().setWidth(20),
-                child: CircularProgressIndicator()
+                child: CircularProgressIndicator(),
               ),
             );
+            
           } else {
+          
             return Text(snapshot.data.docs[0].data()['test']);
+            
           }
         },
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:ToDo/HomeScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
