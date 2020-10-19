3a2e0bc32dcd48cd0a77e45a0373a65f752c9733
Rishi Balamurugan <rishi.b2372@gmail.com>
2020-10-19 16:28:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ToDo
+ A classic todo app
---
+++ android/app/src/main/kotlin/com/example/ToDo/MainActivity.kt
@@ -0,0 +1,6 @@
+package rishi.balamurugan.ToDo.classic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
